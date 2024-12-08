cebe1060fc3c9c526e5acadb06da7988900f66ba
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-12-08 15:42:56 +0530

    Added store for redux and wrapped App with provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
