1b68058ac0cd478e7dc46fbe9bf4d1ad8ce56c42
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-12-08 17:38:53 +0530

    Added toast component to root

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
+import { Toaster } from "./components/ui/toaster.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <App />
+        <Toaster />
       </ThemeProvider>
     </Provider>
   </StrictMode>
