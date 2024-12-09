9c5061bc9f6736ca8db1811e437823d93a4758b8
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-12-09 23:42:07 +0530

    Added get booking endpoint fetch call to booking service

--- src/api/api-services/booking-service.ts
+++ src/api/api-services/booking-service.ts
@@ -1,4 +1,24 @@
 import axiosInstance from "../axios-instance";
+import { User } from "./authentication-service";
+import { Location } from "./location-service";
+
+export type Booking = {
+  bookedBy: User;
+  bookingPrice: number;
+  tripId: string;
+  seats: number;
+  bookingFrom: Location;
+  bookingTo: Location;
+  bookingStatus:
+    | "reserved"
+    | "confirmed"
+    | "cancelled"
+    | "abandoned"
+    | "completed";
+  _id: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
 interface BookingResponse {
   message: string;
@@ -7,13 +27,18 @@ interface BookingResponse {
     bookingPrice: number;
     tripId: string;
     seats: number;
-    bookingFrom: string;
-    bookingTo: string;
+    bookingFrom: Location;
+    bookingTo: Location;
     bookingStatus: "reserved" | "confirmed" | "cancelled" | "abandoned";
     _id: string;
     createdAt: Date;
     updatedAt: Date;
   };
+}
+
+interface AllBookingResponse {
+  message: string;
+  data: Booking[];
 }
 
 const basePath = "/booking";
@@ -38,6 +63,25 @@ export const createBooking = async (
     if (error.response) {
       throw new Error(
         error.response.data.message || "Booking creation failed."
+      );
+    } else if (error.request) {
+      throw new Error("Network error. Please try again later.");
+    } else {
+      throw new Error("An unexpected error occurred.");
+    }
+  }
+};
+
+export const getBookings = async (id: string): Promise<AllBookingResponse> => {
+  try {
+    const response = await axiosInstance.get<AllBookingResponse>(
+      `${basePath}/${id}`
+    );
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw new Error(
+        error.response.data.message || "Failed to retrive bookings."
       );
     } else if (error.request) {
       throw new Error("Network error. Please try again later.");
