b2cc6761bc8e4a9e5cb5aed7009b06a647f0b663
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-12-10 23:55:51 +0530

    Added driver home page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Profile from "./modules/client/dashboard/profile";
 import Search from "./modules/client/dashboard/search";
 import Ride from "./modules/client/dashboard/ride";
 import TrackBooking from "./modules/client/dashboard/track-booking";
+import DriverWelcome from "./modules/driver/base";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -41,6 +42,10 @@ const App = () => {
         {
           path: "/track-booking",
           element: <TrackBooking />,
+        },
+        {
+          path: "/driver/dashboard",
+          element: <DriverWelcome />,
         },
       ],
     },
---
+++ src/modules/driver/base/index.tsx
@@ -0,0 +1,5 @@
+const DriverWelcome = () => {
+  return <div>index</div>;
+};
+
+export default DriverWelcome;
